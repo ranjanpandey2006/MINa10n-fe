e1798f0c387a32ffe092a3693974ed92994be8d8
velders <jeroen@vsdev.nl>
2018-02-16 00:43:18 +0100

    MIN Added two switch buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor (){};
+
+  public switch (event) {
+    const target = event.target || event.srcElement || event.currentTarget;
+    const idAttr = target.attributes.id;
+    const value = idAttr.nodeValue;
+    if (target.getAttribute('status') === 'OFF') {
+        target.setAttribute('status', 'ON');
+    } else {
+        target.setAttribute('status', 'OFF');
+    }
+    console.log(value + ':' + target.getAttribute('status'));
+  }
 }
